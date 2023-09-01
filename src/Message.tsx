835ca03852459cdbc2dd8f25ca7a0c7eba66c8d0
199ocero <199ocero@gmail.com>
2023-09-01 21:10:41 +0800

    added tailwind css

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,7 +1,8 @@
 function Message() {
   const name = "";
-  if (name) return <h1>Hello {name}</h1>;
-  return <h1>Hello World</h1>;
+  if (name)
+    return <h1 className="text-red-500 text-3xl font-bold">Hello {name}</h1>;
+  return <h1 className="text-red-500 text-3xl font-bold">Hello World</h1>;
 }
 
 export default Message;
