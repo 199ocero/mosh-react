2c5e5372c226974480648afcd53157507f3772c8
199ocero <199ocero@gmail.com>
2023-09-03 11:14:02 +0800

    added a state to track the selected item

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
+
 function ListGroup() {
   let items = ["Italy", "France", "United Kingdom", "Netherlands"];
-
+  const [selectedIndex, setSelectedIndex] = useState(-1);
   return (
     <>
       <h1 className="text-3xl my-2 font-bold text-gray-700 dark:text-white">
@@ -10,11 +12,15 @@ function ListGroup() {
         <p className="dark:text-white text-gray-700">No items found.</p>
       )}
       <ul className="w-full text-sm font-medium text-gray-900 bg-white  dark:bg-gray-700 dark:border-gray-600 dark:text-white">
-        {items.map((item) => (
+        {items.map((item, index) => (
           <li
             key={item}
-            onClick={() => console.log(item)}
-            className="w-full px-4 py-2 border-b border-gray-200 dark:border-gray-600"
+            onClick={() => setSelectedIndex(index)}
+            className={
+              selectedIndex === index
+                ? "list-group-item-active"
+                : "list-group-item"
+            }
           >
             {item}
           </li>
