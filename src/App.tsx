35211835580dbe74218e4bbcaf8bb1d81ea9085e
199ocero <199ocero@gmail.com>
2023-09-03 11:42:40 +0800

    handle multiple components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import ListGroup from "./components/ListGroup";
+import { Alert } from "./components/Alert";
 function App() {
   let items = ["Italy", "France", "United Kingdom", "Netherlands"];
 
   return (
-    <div>
-      <ListGroup items={items} heading="Countries" />
-    </div>
+    <>
+      <div className="space-y-5">
+        <div>
+          <ListGroup items={items} heading="Countries" />
+        </div>
+        <div>
+          <Alert />
+        </div>
+      </div>
+    </>
   );
 }
 
