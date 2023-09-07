7c467d85860d86307aebdbe0c1d417f146e396bb
199ocero <199ocero@gmail.com>
2023-09-07 18:38:34 +0800

    state update exercise

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,51 @@ import { useState } from "react";
 function App() {
   let items = ["Italy", "France", "United Kingdom", "Netherlands"];
   const [showAlert, setShowAlert] = useState(false);
+  const [game, setGame] = useState({
+    id: 1,
+    player: {
+      name: "John Doe",
+    },
+  });
+  const [pizza, setPizza] = useState({
+    name: "Spicy Margherita",
+    toppings: ["Mushroom"],
+  });
+  const [cart, setCart] = useState({
+    discount: 0.1,
+    items: [
+      {
+        id: 1,
+        title: "Product 1",
+        quantity: 1,
+      },
+      {
+        id: 2,
+        title: "Product 2",
+        quantity: 1,
+      },
+    ],
+  });
 
   const handleShowAlert = () => {
     setShowAlert(!showAlert);
+  };
+
+  const handleGame = () => {
+    setGame({ ...game, player: { ...game.player, name: "Jane Doe" } });
+  };
+
+  const handlePizza = () => {
+    setPizza({ ...pizza, toppings: [...pizza.toppings, "Onion"] });
+  };
+
+  const handleCart = () => {
+    setCart({
+      ...cart,
+      items: cart.items.map((item) => {
+        return item.id === 1 ? { ...item, quantity: item.quantity + 1 } : item;
+      }),
+    });
   };
 
   return (
@@ -26,6 +68,32 @@ function App() {
             </span>
           </Alert>
         )}
+        <div>
+          <p className="text-gray-700 dark:text-white">
+            This is the original player name: {game.player.name}
+          </p>
+          <Button buttonType="info" onClick={handleGame}>
+            Change Player Name
+          </Button>
+
+          <p className="text-gray-700 dark:text-white">
+            This is the pizza toppings:{" "}
+            {pizza.toppings.map((topping) => {
+              return topping + " ";
+            })}
+          </p>
+          <Button buttonType="info" onClick={handlePizza}>
+            Add Toppings
+          </Button>
+
+          <p className="text-gray-700 dark:text-white">
+            This is total quantity:{" "}
+            {cart.items.map((item) => item.quantity).reduce((a, b) => a + b)}
+          </p>
+          <Button buttonType="info" onClick={handleCart}>
+            Add Quantity
+          </Button>
+        </div>
       </div>
     </>
   );
